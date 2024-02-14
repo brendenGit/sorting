22cf1b8d6ff2db4b3378994ea57475e083d0af24
brenden <brendenjarias@gmail.com>
2024-02-14 12:21:58 -0800

    done

--- quick.js
+++ quick.js
@@ -3,12 +3,31 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, startIdx, endIdx) {
+    const pivot = arr[startIdx];
+    let swapIdx = startIdx;
 
-/*
-quickSort accepts an array, left index, and right index
-*/
+    for (let i = startIdx + 1; i <= endIdx; i++) {
+        if (pivot > arr[i]) {
+            swapIdx++;
+            [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
+        }
+    }
 
-function quickSort() {}
+    [arr[startIdx], arr[swapIdx]] = [arr[swapIdx], arr[startIdx]];
+    return swapIdx;
+}
+
+function quickSort(arr, leftIdx = 0, rightIdx = arr.length - 1) {
+    if (leftIdx < rightIdx) {
+        let pivotIndex = pivot(arr, leftIdx, rightIdx);
+        quickSort(arr, leftIdx, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, rightIdx);
+    }
+    return arr;
+}
+
+const test = quickSort([10,5,6,2,22,11,3])
+console.log(test);
 
 module.exports = quickSort;
--- radix.js
+++ radix.js
@@ -1,9 +1,22 @@
 function radixSort(arr) {
-    const buckets = [[], [], [], [], [], [], [], [], [], [],];
-    
+    const buckets = [[], [], [], [], [], [], [], [], [], []];
+    let results = arr.slice(); // Copy the array to avoid modifying the original
+    const maxDigits = Math.max(...arr).toString().length;
+
+    for (let p = 0; p < maxDigits; p++) {
+        // Clear buckets on each iteration
+        buckets.forEach(bucket => bucket.length = 0);
+        for (let number of results) {
+            const digit = getDigit(number, p);
+            buckets[digit].push(number);
+        }
+        results = [].concat(...buckets);
+    }
+    return results;
 }
 
-const result = radixSort([0,1,2,3,5,78])
-console.log(result);
+function getDigit(number, place) {
+    return Math.floor(Math.abs(number) / Math.pow(10, place)) % 10;
+}
 
 module.exports = radixSort;
