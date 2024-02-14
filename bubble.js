c1fddaacbb9614e50044984c982b1bce2162229c
brenden <brendenjarias@gmail.com>
2024-02-14 09:16:14 -0800

    creatd repo, built bubble sort

---
+++ bubble.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+    //bubble up an item to the back of the list if it is higher'
+    let swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+                swapped = true;
+            }
+        }
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
---
+++ merge.js
@@ -0,0 +1,27 @@
+function merge() {
+    const results = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            results.push(arr1[i])
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+}
+
+function mergeSort() { }
+
+module.exports = { merge, mergeSort };
