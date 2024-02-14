f94ed8f5aec9b11385ce72018e7fb87be66d69e6
brenden <brendenjarias@gmail.com>
2024-02-14 11:11:24 -0800

    completed part of further study and all required study

--- insertion.js
+++ insertion.js
@@ -1,5 +1,17 @@
-function insertionSort() {
-    
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let temp = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+    return arr;
 }
+
+const results = insertionSort([4, 5, 6, 7, 1]);
+console.log(results);
 
 module.exports = insertionSort;
--- radix.js
+++ radix.js
@@ -1,3 +1,9 @@
-function radixSort() {}
+function radixSort(arr) {
+    const buckets = [[], [], [], [], [], [], [], [], [], [],];
+    
+}
+
+const result = radixSort([0,1,2,3,5,78])
+console.log(result);
 
 module.exports = radixSort;
